f3e180470f4f43ea74349bb433b13f5f8806e374
joschrew <jonas.schrewe@gwdg.de>
2022-02-22 16:53:31 +0100

    correct path for ExportController.exportFile()

--- src/main/java/ola/hd/longtermstorage/controller/ExportController.java
+++ src/main/java/ola/hd/longtermstorage/controller/ExportController.java
@@ -240,7 +240,7 @@ public class ExportController {
         @ApiResponse(code = 200, message = "File was found.", response = byte[].class),
         @ApiResponse(code = 404, message = "An archive with the specified identifier or the requested file the was not found.", response = ResponseMessage.class),
     })
-    @GetMapping(value = "/export/", produces = { MediaType.APPLICATION_OCTET_STREAM_VALUE })
+    @GetMapping(value = "/export/file", produces = { MediaType.APPLICATION_OCTET_STREAM_VALUE })
     public ResponseEntity<InputStreamResource> exportFile(
             @ApiParam(value = "The PID/PPA of the work.", required = true) @RequestParam String id,
             @ApiParam(value = "Path to file.", required = true) @RequestParam String path) throws IOException {
