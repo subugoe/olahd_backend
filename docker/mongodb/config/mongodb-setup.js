47a925ca44a8a50093313f976cb47ae40b3395b4
Joerg Panzer <panzer@sub.uni-goettingen.de>
2022-04-05 13:22:11 +0200

    Merge states

---
+++ docker/mongodb/config/mongodb-setup.js
@@ -0,0 +1,8 @@
+db.createUser(
+    {
+        user: "$MONGO_INITDB_ROOT_USERNAME",
+        pwd: "$MONGO_INITDB_ROOT_PASSWORD",
+        roles: [
+          { role: "readWrite", db: "$MONGO_INITDB_DATABASE" }
+        ]
+    })
