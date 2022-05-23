42a18f6539128b208d3bb50480d98f4d755cce02
joschrew <jonas.schrewe@gwdg.de>
2022-05-23 08:10:03 +0200

    update startup script

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+cp -n src/main/resources/application.properties.template src/main/resources/application.properties
+cp -n src/main/resources/application-development.properties.template src/main/resources/application-development.properties
 docker-compose -f docker-compose.base.yaml -f docker-compose.local.yaml -f docker-compose.kibana.yaml down -v
 docker-compose -f docker-compose.base.yaml -f docker-compose.local.yaml -f docker-compose.kibana.yaml pull
 docker-compose -f docker-compose.base.yaml -f docker-compose.local.yaml -f docker-compose.kibana.yaml build --no-cache # olahds_search
