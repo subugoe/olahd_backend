a82b5468b48405aad8ab8f3049582711ef8539ee
joschrew <jonas.schrewe@gwdg.de>
2022-10-13 09:50:04 +0200

    reformat code (whitespace) for model
    we should stick to indent with 4 spaces. Removed annotations as currently not used

--- src/main/java/ola/hd/longtermstorage/model/HitList.java
+++ src/main/java/ola/hd/longtermstorage/model/HitList.java
@@ -1,114 +1,99 @@
 package ola.hd.longtermstorage.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-
-@Document(indexName = "hitlist", type = "hitlist")
 public class HitList {
-		@Field(type = FieldType.Text)
-		private String PID;
-		@Id
-		@Field(type = FieldType.Keyword)
-		private String ID;
-		@Field(type = FieldType.Text)
-		private String title;
-		@Field(type = FieldType.Text)
-		private String subtitle;
-		@Field(type = FieldType.Text)
-		private String placeOfPublish;
-		@Field(type = FieldType.Integer)
-		private int yearOfPublish;
-		@Field(type = FieldType.Text)
-		private String publisher;
-		@Field(type = FieldType.Text)
-		private String creator;
-	    private FulltextSnippets fulltextSnippets;
+    private String PID;
+    private String ID;
+    private String title;
+    private String subtitle;
+    private String placeOfPublish;
+    private int yearOfPublish;
+    private String publisher;
+    private String creator;
+    private FulltextSnippets fulltextSnippets;
 
-	public HitList(String PID, String ID, String title, String subtitle, String placeOfPublish, int yearOfPublish, String publisher, String creator, FulltextSnippets fulltextSnippets) {
-		this.PID = PID;
-		this.ID = ID;
-		this.title = title;
-		this.subtitle = subtitle;
-		this.placeOfPublish = placeOfPublish;
-		this.yearOfPublish = yearOfPublish;
-		this.publisher = publisher;
-		this.creator = creator;
-		this.fulltextSnippets = fulltextSnippets;
-	}
+    public HitList(String PID, String ID, String title, String subtitle, String placeOfPublish, int yearOfPublish,
+            String publisher, String creator, FulltextSnippets fulltextSnippets) {
+        this.PID = PID;
+        this.ID = ID;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.placeOfPublish = placeOfPublish;
+        this.yearOfPublish = yearOfPublish;
+        this.publisher = publisher;
+        this.creator = creator;
+        this.fulltextSnippets = fulltextSnippets;
+    }
 
-	public String getPID() {
-		return PID;
-	}
+    public String getPID() {
+        return PID;
+    }
 
-	public void setPID(String PID) {
-		this.PID = PID;
-	}
+    public void setPID(String PID) {
+        this.PID = PID;
+    }
 
-	public String getID() {
-		return ID;
-	}
+    public String getID() {
+        return ID;
+    }
 
-	public void setID(String ID) {
-		this.ID = ID;
-	}
+    public void setID(String ID) {
+        this.ID = ID;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getSubtitle() {
-		return subtitle;
-	}
+    public String getSubtitle() {
+        return subtitle;
+    }
 
-	public void setSubtitle(String subtitle) {
-		this.subtitle = subtitle;
-	}
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
 
-	public String getPlaceOfPublish() {
-		return placeOfPublish;
-	}
+    public String getPlaceOfPublish() {
+        return placeOfPublish;
+    }
 
-	public void setPlaceOfPublish(String placeOfPublish) {
-		this.placeOfPublish = placeOfPublish;
-	}
+    public void setPlaceOfPublish(String placeOfPublish) {
+        this.placeOfPublish = placeOfPublish;
+    }
 
-	public int getYearOfPublish() {
-		return yearOfPublish;
-	}
+    public int getYearOfPublish() {
+        return yearOfPublish;
+    }
 
-	public void setYearOfPublish(int yearOfPublish) {
-		this.yearOfPublish = yearOfPublish;
-	}
+    public void setYearOfPublish(int yearOfPublish) {
+        this.yearOfPublish = yearOfPublish;
+    }
 
-	public String getPublisher() {
-		return publisher;
-	}
+    public String getPublisher() {
+        return publisher;
+    }
 
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
-	}
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 
-	public String getCreator() {
-		return creator;
-	}
+    public String getCreator() {
+        return creator;
+    }
 
-	public void setCreator(String creator) {
-		this.creator = creator;
-	}
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
 
-	public FulltextSnippets getFulltextSnippets() {
-		return fulltextSnippets;
-	}
+    public FulltextSnippets getFulltextSnippets() {
+        return fulltextSnippets;
+    }
 
-	public void setFulltextSnippets(FulltextSnippets fulltextSnippets) {
-		this.fulltextSnippets = fulltextSnippets;
-	}
+    public void setFulltextSnippets(FulltextSnippets fulltextSnippets) {
+        this.fulltextSnippets = fulltextSnippets;
+    }
 
 }
-
--- src/main/java/ola/hd/longtermstorage/model/ResultSet.java
+++ src/main/java/ola/hd/longtermstorage/model/ResultSet.java
@@ -1,101 +1,92 @@
 package ola.hd.longtermstorage.model;
 
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+import java.util.List;
 
-@Document(indexName = "resultSet", type = "resultSet")
 public class ResultSet {
 
-		@Field(type = FieldType.Integer)
-		private int hits;
-		@Field(type = FieldType.Integer)
-		private int offset;
-		@Field(type = FieldType.Integer)
-		private	int limit;
-		@Field(type = FieldType.Boolean)
-		private	boolean metadataSearch;
-		@Field(type = FieldType.Boolean)
-		private	boolean fulltextSearch;
-	 	@Field(type = FieldType.Text)
-		private	String searchTerm;
-		@Field(type = FieldType.Text)
-		private HitList hitlist;
-		@Field(type = FieldType.Text)
-		private Facets facets;
+    private int hits;
+    private int offset;
+    private int limit;
+    private boolean metadataSearch;
+    private boolean fulltextSearch;
+    private String searchTerm;
+    private List<HitList> hitlist;
+    private List<Facets> facets;
 
-	    public ResultSet(String searchTerm, int hits, int offset, int limit, boolean metadataSearch, boolean fulltextSearch, HitList hitlist, Facets facets ) {
-	        this.hits = hits;
-	        this.offset = offset;
-	        this.limit = limit;
-			this.metadataSearch = metadataSearch;
-			this.fulltextSearch = fulltextSearch;
-			this.searchTerm = searchTerm;
-			this.hitlist = hitlist;
-			this.facets = facets;
-		}
+    public ResultSet(String searchTerm, int hits, int offset, int limit, boolean metadataSearch,
+            boolean fulltextSearch, List<HitList> hitlist, List<Facets> facets) {
+        this.hits = hits;
+        this.offset = offset;
+        this.limit = limit;
+        this.metadataSearch = metadataSearch;
+        this.fulltextSearch = fulltextSearch;
+        this.searchTerm = searchTerm;
+        this.hitlist = hitlist;
+        this.facets = facets;
+    }
 
-		public int getHits() {
-			return hits;
-		}
+    public int getHits() {
+        return hits;
+    }
 
-		public void setHits(int hits) {
-			this.hits = hits;
-		}
+    public void setHits(int hits) {
+        this.hits = hits;
+    }
 
-		public int getOffset() {
-			return offset;
-		}
+    public int getOffset() {
+        return offset;
+    }
 
-		public void setOffset(int offset) {
-			this.offset = offset;
-		}
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
 
-		public int getLimit() {
-			return limit;
-		}
+    public int getLimit() {
+        return limit;
+    }
 
-		public void setLimit(int limit) {
-			this.limit = limit;
-		}
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 
-		public boolean isMetadataSearch() {
-			return metadataSearch;
-		}
+    public boolean isMetadataSearch() {
+        return metadataSearch;
+    }
 
-		public void setMetadataSearch(boolean metadataSearch) {
-			this.metadataSearch = metadataSearch;
-		}
+    public void setMetadataSearch(boolean metadataSearch) {
+        this.metadataSearch = metadataSearch;
+    }
 
-		public boolean isFulltextSearch() {
-			return fulltextSearch;
-		}
+    public boolean isFulltextSearch() {
+        return fulltextSearch;
+    }
 
-		public void setFulltextSearch(boolean fulltextSearch) {
-			this.fulltextSearch = fulltextSearch;
-		}
+    public void setFulltextSearch(boolean fulltextSearch) {
+        this.fulltextSearch = fulltextSearch;
+    }
 
-		public String getSearchTerm() {
-			return searchTerm;
-		}
+    public String getSearchTerm() {
+        return searchTerm;
+    }
 
-		public void setSearchTerm(String searchTerm) {
-			this.searchTerm = searchTerm;
-		}
+    public void setSearchTerm(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
 
-		public HitList getHitlist() {
-			return hitlist;
-		}
+    public List<HitList> getHitlist() {
+        return hitlist;
+    }
 
-		public void setHitlist(HitList hitlist) {
-			this.hitlist = hitlist;
-		}
+    public void setHitlist(List<HitList> hitlist) {
+        this.hitlist = hitlist;
+    }
 
-		public Facets getFacets() {
-			return facets;
-		}
+    public List<Facets> getFacets() {
+        return facets;
+    }
 
-		public void setFacets(Facets facets) {
-			this.facets = facets;
-		}
+    public void setFacets(List<Facets> facets) {
+        this.facets = facets;
+    }
+
 }
