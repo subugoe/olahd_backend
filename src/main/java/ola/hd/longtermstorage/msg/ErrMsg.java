5fe97a5b4bda33f33130e60c6c2c065ad85c9e35
joschrew <jonas.schrewe@gwdg.de>
2022-10-13 09:51:18 +0200

    changed text error messages

--- src/main/java/ola/hd/longtermstorage/msg/ErrMsg.java
+++ src/main/java/ola/hd/longtermstorage/msg/ErrMsg.java
@@ -12,9 +12,10 @@ public class ErrMsg {
     public static final String FILE_NOT_FOUND = "File not found";
     public static final String PARAM_ID_IS_EMPTY = "Parameter id may not be empty";
     public static final String PARAM_PATH_IS_EMPTY = "Parameter path may not be empty";
-    public static final String ID_NOT_FOUND = "no archive available for provided id";
-    public static final String METS_NOT_FOUND = "mets-file not found in archive";
+    public static final String ID_NOT_FOUND = "No archive available for provided id";
+    public static final String METS_NOT_FOUND = "Mets-file not found in archive";
     public static final String RECORD_NOT_FOUND = "Record not found";
-    public static final String ID_OR_TERM_MISSING = "One of the Request parameters id and searchterm is required";
+    public static final String ID_OR_TERM_MISSING = "One of the request parameters id or searchterm are required";
+    public static final String FULL_OR_METASEARCH = "Either 'metadatasearch' or 'fulltextsearch' must be true";
 
 }
