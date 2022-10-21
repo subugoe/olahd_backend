f6328dd589e2ad1262a60b5bae5d19c1f3c5d069
joschrew <jonas.schrewe@gwdg.de>
2022-10-21 11:19:57 +0200

    add new error message

--- src/main/java/ola/hd/longtermstorage/msg/ErrMsg.java
+++ src/main/java/ola/hd/longtermstorage/msg/ErrMsg.java
@@ -1,5 +1,7 @@
 package ola.hd.longtermstorage.msg;
 
+import ola.hd.longtermstorage.elasticsearch.ElasticQueryHelper;
+
 /**
  * Constants for error-messages which are used more than once
  */
@@ -17,5 +19,7 @@ public class ErrMsg {
     public static final String RECORD_NOT_FOUND = "Record not found";
     public static final String ID_OR_TERM_MISSING = "One of the request parameters id or searchterm are required";
     public static final String FULL_OR_METASEARCH = "Either 'metadatasearch' or 'fulltextsearch' must be true";
+    public static final String FIELD_NOT_EQUALS_VALUE = "'field' and 'value' must be given the same number of times";
+    public static final String UNKNOWN_FILTER = "'field' contains unknown filter. Valid are: " + String.join(", ", ElasticQueryHelper.FILTER_MAP.keySet());
 
 }
