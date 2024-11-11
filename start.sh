cb44302f488a2d7dca1b40648bc20ac212addc55
joschrew <91774427+joschrew@users.noreply.github.com>
2024-11-11 12:01:49 +0100

    Change local docker startup

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
-cp -n src/main/resources/application.properties.template src/main/resources/application.properties
-cp -n src/main/resources/application-development.properties.template src/main/resources/application-development.properties
+#!/bin/bash -e
+cp -n src/main/resources/application.properties.local src/main/resources/application.properties
 docker-compose -f docker-compose.base.yaml -f docker-compose.local.yaml -f docker-compose.kibana.yaml down -v
 docker-compose -f docker-compose.base.yaml -f docker-compose.local.yaml -f docker-compose.kibana.yaml pull
 docker-compose -f docker-compose.base.yaml -f docker-compose.local.yaml -f docker-compose.kibana.yaml build --no-cache # olahds_search
 docker-compose -f docker-compose.base.yaml -f docker-compose.local.yaml -f docker-compose.kibana.yaml up -d # olahds_search
-docker-compose -f docker-compose.base.yaml -f docker-compose.local.yaml -f docker-compose.kibana.yaml logs -f # olahds_search
